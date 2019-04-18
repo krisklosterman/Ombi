924342249eb470dbb70e34bc1be009da7fc99bce
Kristopher Klosterman <siriuskr@gmail.com>
2019-04-18 17:01:01 -0400

    Search Movies was not using stylesURL, changed it to use it

--- src/Ombi/ClientApp/app/search/search.component.ts
+++ src/Ombi/ClientApp/app/search/search.component.ts
@@ -5,6 +5,7 @@ import { IssuesService, SettingsService } from "../services";
 
 @Component({
     templateUrl: "./search.component.html",
+    styleUrls: ["./search.component.scss"]
 })
 export class SearchComponent implements OnInit  {
     public showTv: boolean;
